3f2146f02d06b2d861fbb70f2db9df06d58f9698
Aanya Tanwar <aanya.tanwar@simformsolutions.com>
2025-02-07 12:53:29 +0530

    Update repo_changes-update.sh

--- repo_changes-update.sh
+++ repo_changes-update.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Define your repository and log file paths
 REPO_DIR="/home/aanya.tanwar@simform.dom/repo-changes-update"
 LOG_FILE="/home/aanya.tanwar@simform.dom/git_update.log"
-
-# Change to the repository directory
 cd $REPO_DIR
 
-# Fetch the latest changes
 git fetch origin >> "$LOG_FILE" 2>&1
 
-# Check if the branch is behind
 git status | grep -q 'Your branch is behind'
 
 if [ $? -eq 0 ]; then
